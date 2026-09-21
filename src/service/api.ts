872fa45385635d484fd0f99fc53f43121b206683
Steven <22393225@utcancun.edu.mx>
2026-09-21 09:33:35 -0500

    Conexion com API terminada

---
+++ src/service/api.ts
@@ -0,0 +1,26 @@
+export interface Monsters{
+    id: number;
+    name: string;
+    image: string;
+    description: string;
+}
+
+interface CreatureResponse {
+    data: Monsters[];
+    message: string;
+    status: number;
+}
+
+const API_URL = "https://api.hyrule-compendium.com/v3/compendium/category/monsters";
+
+export async function getMonsters(): Promise<Monsters[]> {
+    const response = await fetch(API_URL);
+
+    if (!response.ok) {
+        throw new Error("Error al obtener datos");
+    }
+
+    const json: CreatureResponse = await response.json();
+
+    return json.data;
+}
